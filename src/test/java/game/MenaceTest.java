e40bca70fb2ad8a5653d129e36c91ddb24a689c0
Revanth Katha <revanthkatha@Revanths-MacBook-Air.local>
2022-04-27 15:47:05 -0400

    added test case for menace

--- src/test/java/game/MenaceTest.java
+++ src/test/java/game/MenaceTest.java
@@ -1,4 +1,25 @@
 package game;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MenaceTest {
+
+    @Test
+    void testCreateKey() {
+        List<Integer> t1 = Arrays.asList(0, 0, 0);
+        List<Integer> t2 = Arrays.asList(-1, -1);
+        Menace m = new Menace(3);
+        String s = "012212121";
+        m.createKey(s);
+        assertEquals(m.getBotBoards().get(s), t1);
+        s = "212212121";
+        m.createKey(s);
+        assertEquals(m.getBotBoards().get(s), t2);
+    }
+
+
 }
