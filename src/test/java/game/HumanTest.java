447bf7efa57950591b9a38e24b5b449e0035cc26
Revanth Katha <revanthkatha@Revanths-MacBook-Air.local>
2022-04-27 15:13:16 -0400

    added test case for converting string to board

--- src/test/java/game/HumanTest.java
+++ src/test/java/game/HumanTest.java
@@ -1,4 +1,25 @@
 package game;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HumanTest {
+
+    @Test
+    void testConvertStringtoBoard() {
+        int[][] b = {
+                {1, 2, 0},
+                {2, 1, 0},
+                {0, 0, 0}
+        };
+        Human h = new Human();
+        String s = "120210000";
+        int[][] a = h.convertStringtoBoard(s);
+        assertTrue(Arrays.deepEquals(b, a));
+    }
+
+
 }
