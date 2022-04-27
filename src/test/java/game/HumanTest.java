8a316884e6bb7aff24b584e0a51180de63735c5e
Revanth Katha <revanthkatha@Revanths-MacBook-Air.local>
2022-04-27 15:22:49 -0400

    addded test case for check win

--- src/test/java/game/HumanTest.java
+++ src/test/java/game/HumanTest.java
@@ -21,5 +21,37 @@ public class HumanTest {
         assertTrue(Arrays.deepEquals(b, a));
     }
 
-
+    @Test
+    void testCheckwin() {
+        Human h = new Human();
+        assertEquals(h.checkwin(-1, 2), 0);
+        h.board[0][0] = 2;
+        h.board[0][1] = 1;
+        h.board[0][2] = 0;
+        h.board[1][0] = 1;
+        h.board[1][1] = 2;
+        h.board[1][2] = 0;
+        h.board[2][0] = 0;
+        h.board[2][1] = 0;
+        h.board[2][2] = 2;
+        assertEquals(2, h.checkwin(8, 2));
+        h.board[1][0] = 2;
+        h.board[1][1] = 1;
+        h.board[2][0] = 2;
+        h.board[2][2] = 0;
+        assertEquals(2, h.checkwin(6, 2));
+        h.board[0][1] = 2;
+        h.board[0][2] = 2;
+        h.board[1][0] = 1;
+        h.board[2][0] = 0;
+        assertEquals(2, h.checkwin(2, 2));
+        h.board[0][1] = 1;
+        h.board[1][1] = 2;
+        h.board[2][0] = 2;
+        h.board[0][0] = 1;
+        assertEquals(2, h.checkwin(2, 2));
+        h.board[1][0] = 2;
+        h.board[2][0] = 1;
+        assertEquals(0, h.checkwin(6, 2));
+    }
 }
