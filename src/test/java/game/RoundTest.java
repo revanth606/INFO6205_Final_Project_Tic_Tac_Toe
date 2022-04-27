82b76bda8e375d80c8bdb2013c181f2966f3bf24
Jashwanth Kamsani <jessi@Jashwanths-MBP.hsd1.ma.comcast.net>
2022-04-27 15:29:33 -0400

    Added unit tests to test checkwin() in Round class

--- src/test/java/game/RoundTest.java
+++ src/test/java/game/RoundTest.java
@@ -18,4 +18,40 @@ public class RoundTest {
         String s = "011210220";
         assertEquals(s, r.convertBoardtoString(b));
     }
+
+    @Test
+    void testCheckwin() {
+        Human h = new Human();
+        Menace m = new Menace(3);
+        Round r = new Round(m, h);
+        assertEquals(r.checkwin(-1, 2), 0);
+        r.board[0][0] = 2;
+        r.board[0][1] = 1;
+        r.board[0][2] = 0;
+        r.board[1][0] = 1;
+        r.board[1][1] = 2;
+        r.board[1][2] = 0;
+        r.board[2][0] = 0;
+        r.board[2][1] = 0;
+        r.board[2][2] = 2;
+        assertEquals(2, r.checkwin(8, 2));
+        r.board[1][0] = 2;
+        r.board[1][1] = 1;
+        r.board[2][0] = 2;
+        r.board[2][2] = 0;
+        assertEquals(2, r.checkwin(6, 2));
+        r.board[0][1] = 2;
+        r.board[0][2] = 2;
+        r.board[1][0] = 1;
+        r.board[2][0] = 0;
+        assertEquals(2, r.checkwin(2, 2));
+        r.board[0][1] = 1;
+        r.board[1][1] = 2;
+        r.board[2][0] = 2;
+        r.board[0][0] = 1;
+        assertEquals(2, r.checkwin(2, 2));
+        r.board[1][0] = 2;
+        r.board[2][0] = 1;
+        assertEquals(0, r.checkwin(6, 1));
+    }
 }
