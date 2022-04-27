3a8fea6e71f95367894bc5bd8974920f2c70dd08
Revanth Katha <revanthkatha@Revanths-MacBook-Air.local>
2022-04-27 15:58:27 -0400

    added test case for turn in menace

--- src/test/java/game/MenaceTest.java
+++ src/test/java/game/MenaceTest.java
@@ -21,5 +21,18 @@ public class MenaceTest {
         assertEquals(m.getBotBoards().get(s), t2);
     }
 
+    @Test
+    void testTurn() {
+        List<Integer> t1 = Arrays.asList(0, 0);
+        List<Integer> t2 = Arrays.asList(0);
+        Menace m = new Menace(3);
+        String s = "012212121";
+        m.turn(s);
+        assertEquals(m.getBotBoards().get(s), t1);
+        m.turn(s);
+        assertEquals(m.getBotBoards().get(s), t2);
+        m.turn(s);
+        assertEquals(m.getBotBoards().get(s), t2);
+    }
 
 }
