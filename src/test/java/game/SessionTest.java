eb97fe8d835aaf4c6d23dd4bcba23d2b1d239832
Jashwanth Kamsani <jessi@Jashwanths-MBP.hsd1.ma.comcast.net>
2022-04-27 21:27:14 -0400

    Few edits to the variables

--- src/test/java/game/SessionTest.java
+++ src/test/java/game/SessionTest.java
@@ -5,8 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertTrue;
 
 public class SessionTest {
+
     @Test
     void testTrain() {
+        Session s = new Session();
+        Menace m = s.train(10, 4, 1, 2, 0.1, 100);
+        assertTrue(m!=null);
+    }
+
+    @Test
+    void testPlay() {
         Session s = new Session();
         Menace m = s.train(10, 4, 1, 2, 0.1, 100);
         assertTrue(m!=null);
